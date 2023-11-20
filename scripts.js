757b559af67393ee398239389959fee458e5d83a
MyPetLobster <6979547+MyPetLobster@users.noreply.github.com>
2023-11-20 18:43:08 -0500

    Add functionality to grid color fill

--- scripts.js
+++ scripts.js
@@ -1,18 +1,33 @@
 const gridContainer = document.querySelector(".container");
 
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
 function drawGrid(x) {
 
     let pixelSize = (100/x)
 
     for (let i = 0; i < (x * x); i++) {
-        let newDiv = document.createElement("div");
-        newDiv.classList.add("pixel");
-        newDiv.style.width = `${pixelSize}%`;
-        newDiv.style.height = `${pixelSize}%`;
-        gridContainer.appendChild(newDiv);
+        let gridItem = document.createElement("div");
+        gridItem.classList.add("pixel");
+        gridItem.style.width = `${pixelSize}%`;
+        gridItem.style.height = `${pixelSize}%`;
+        gridItem.addEventListener('mouseover', fillItem)
+        gridContainer.appendChild(gridItem);
+
         
+        
+    }
+}
+
+function fillItem(e) {
+    if (e.type === "mouseover" && mouseDown) {
+        e.target.style.backgroundColor = currentColor;
     }
 
 }
 
+
+currentColor = "blue";
 drawGrid(60);
