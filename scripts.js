0aa4b6820a8f998fe43a4b2a19c2fa235d8a1868
MyPetLobster <6979547+MyPetLobster@users.noreply.github.com>
2023-11-21 11:16:49 -0500

    Cleanup, organize code

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@
 // Global Selectors
 const gridContainer = document.querySelector(".grid-container");
 
-// Dynamic Elements 
+// Dynamic Elements (default state)
 let currentSize = 75;
 let currentPenColor = "black";
 
@@ -11,9 +11,10 @@ let mouseDown = false;
 document.body.onmousedown = () => (mouseDown = true);
 document.body.onmouseup = () => (mouseDown = false);
 
+// Draw initial grid when page loads
 window.addEventListener('load', () => {drawGrid(currentSize)});
 
-
+// FUNCTIONS
 function drawGrid(x) {
     let pixelSize = (100/x)
     for (let i = 0; i < (x * x); i++) {
@@ -34,48 +35,30 @@ function changeBackground(color) {
     gridContainer.style.backgroundColor = color;
 }
 
-
-
-
-
-
-
+// Selectors and Actions
 const clearAll = document.querySelector("#clear-all");
-
 clearAll.addEventListener('click', () => {
     gridContainer.innerHTML = '';
     changeBackground("rgb(200, 205, 199)")
     drawGrid(currentSize);
 });
+const colorSelector = document.querySelector("#color-selector");
+colorSelector.addEventListener('change', () => {currentPenColor = colorSelector.value});
+const changeBG = document.querySelector("#bg-color-selector");
+changeBG.addEventListener('change', () => {changeBackground(changeBG.value)});
 
 
-
-
-
-
-
+// Slider Controls and Display Function
 var slider = document.getElementById("myRange");
 var output = document.getElementById("display-size");
-output.innerHTML = slider.value; // Display the default slider value
-
-// Update the current slider value (each time you drag the slider handle)
+output.innerHTML = slider.value; 
 slider.oninput = function() {
     gridContainer.innerHTML = '';
     output.innerHTML = this.value;
     currentSize = 100 - this.value;
-    
     drawGrid(currentSize);
 }
 
 
 
 function gridToggle () {}
-
-function saveScreenshot() {}
-
-
-const colorSelector = document.querySelector("#color-selector");
-colorSelector.addEventListener('change', () => {currentPenColor = colorSelector.value});
-
-const changeBG = document.querySelector("#bg-color-selector");
-changeBG.addEventListener('change', () => {changeBackground(changeBG.value)});
