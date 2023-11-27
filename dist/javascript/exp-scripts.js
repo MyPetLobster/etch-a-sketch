46ccc3c4d3536f3ce99f35858f51e969f82eec82
MyPetLobster <6979547+MyPetLobster@users.noreply.github.com>
2023-11-26 19:35:12 -0500

    Implement all logic for image selection

--- dist/javascript/exp-scripts.js
+++ dist/javascript/exp-scripts.js
@@ -70,49 +70,227 @@ function picasso() {
 }
 
 
-const textField = document.querySelector(".prompt-text");
-const promptForm = document.querySelector(".prompt-form");
-
-
-
-// Submit Prompt
 function handleForm(event) {
     event.preventDefault();
-    const inputValue = textField.value.toLowerCase(); // Convert the input to lowercase for case-insensitive comparison
-    if (inputValue === "mona lisa") {
-        gridCanvas.innerHTML = "";
+    gridCanvas.innerHTML = "";
+    const inputValue = textField.value.toLowerCase().trim();
+    let randomSelect = 0;
+
+    const monaLisa = ["mona lisa", "mona", "art", "painting"];
+    const dog = ["dog", "dogs", "puppy", "puppies"];
+    const joselyn = "joselyn";
+    const daisy = "daisy";
+    const mansBestFriend = ["man's best friend", "mans best friend"];
+    const cat = ["cat", "cats", "kitten", "kitty", "meow"];
+    const bear = ["bear", "scary", "grizzly", "grizzly bear", "wild animal"];
+    const wolf = ["wolf", "wolves", "coyote", "wild animal"];
+    const fish = ["fish", "fishing", "salmon"];
+    const vacation = ["vacation", "relax", "beach", "summer"];
+    const bestFriends = ["bff", "bestfriends", "friend", "friends", "best friends", "lovers"];
+    const animal = ["animals", "animal"];
+    const coolDude = ["cool", "cool dude", "awesome"];
+    const family = ["family", "love", "wife", "husband", "married", "couple", "soulmate"];
+    const guns = ["gun", "guns", "firearm", "firearms", "shoot", "shooting"];
+    
+
+    const nature = ["nature", "scenic", "breathtaking", "outdoor", "outside", "outdoors",
+                    "lake", "pond", "ocean", "river", "water", "mountain", "mountains"];
+
+    const prettyLady = ["pretty", "lady", "pretty lady", "pretty woman", "beautiful", "sexy",
+                        "woman", "hot", "hot woman", "sexy lady", "bombshell", "babe", 
+                        "supermodel"];
+    
+    if (monaLisa.some(keyword => inputValue.includes(keyword))) {
         gridCanvas.style.backgroundImage = "url('../../images/mona-lisa.jpg')";
         gridCanvas.style.backgroundSize = "contain";
-    } else if (inputValue === "dog") {
-        gridCanvas.innerHTML = "";
-        gridCanvas.style.backgroundImage = "url('../../images/dog-1.jpg')";
-        gridCanvas.style.backgroundSize = "contain";  // Fix here
-    } else if (inputValue === "dogs" || inputValue === "cute") {
-        gridCanvas.innerHTML = "";
-        gridCanvas.style.backgroundImage = "url('../../images/joselyn-daisy.jpg')";
-        gridCanvas.style.backgroundSize = "contain";  // Fix here
-    } else if (inputValue === "cat" || inputValue === "kitty") {
-        gridCanvas.innerHTML = "";
-        gridCanvas.style.backgroundImage = "url('../../images/cat-2.jpg')";
-        gridCanvas.style.backgroundSize = "contain";  // Fix here
-    } else if (inputValue === "handsome" || inputValue === "hot") {
-        gridCanvas.innerHTML = "";
-        gridCanvas.style.backgroundImage = "url('../../images/handsome.jpg')";
-        gridCanvas.style.backgroundSize = "contain";  // Fix here
-    } else if (inputValue === "bff" || inputValue === "friends") {
-        gridCanvas.innerHTML = "";
-        gridCanvas.style.backgroundImage = "url('../../images/bff.jpg')";
-        gridCanvas.style.backgroundSize = "contain";  // Fix here
-    } else if (inputValue === "love" || inputValue === "family") {
-        gridCanvas.innerHTML = "";
-        gridCanvas.style.backgroundImage = "url('../../images/family.jpg')";
-        gridCanvas.style.backgroundSize = "contain";  // Fix here
+    } else if (dog.some(keyword => inputValue.includes(keyword))) {
+        randomSelect = Math.floor(Math.random() * 4);
+        if (randomSelect === 0) {
+            gridCanvas.style.backgroundImage = "url('../../images/dog-1.jpg')";
+        } else if (randomSelect === 1 ) {
+            gridCanvas.style.backgroundImage = "url('../../images/dog-2.jpg')";
+        } else if (randomSelect === 2) {
+            gridCanvas.style.backgroundImage = "url('../../images/joselyn-daisy.jpg')";
+        } else {
+            gridCanvas.style.backgroundImage = "url('../../images/joselyn-daisy-2.jpg')";
+        }
+        gridCanvas.style.backgroundSize = "contain";
+    } else if (mansBestFriend.some(keyword => inputValue.includes(keyword))) {
+        randomSelect = Math.floor(Math.random() * 4);
+        if (randomSelect === 0) {
+            gridCanvas.style.backgroundImage = "url('../../images/nick-and-joselyn.jpg')";
+        } else if (randomSelect === 1 ) {
+            gridCanvas.style.backgroundImage = "url('../../images/nick-and-joselyn-2.jpg')";
+        } else if (randomSelect === 2) {
+            gridCanvas.style.backgroundImage = "url('../../images/me-and-daisy.jpg')";
+        } else {
+            gridCanvas.style.backgroundImage = "url('../../images/me-and-daisy-2.jpg')";
+        }
+        gridCanvas.style.backgroundSize = "contain";
+    } else if (vacation.some(keyword => inputValue.includes(keyword))) {
+        gridCanvas.style.backgroundImage = "url('../../images/vacation.jpg')";
+        gridCanvas.style.backgroundSize = "contain";
+    } else if (cat.some(keyword => inputValue.includes(keyword))) {
+        randomSelect = Math.floor(Math.random() * 2);
+        if (randomSelect === 0) {
+            gridCanvas.style.backgroundImage = "url('../../images/cat-1.jpg')";
+        } else {
+            gridCanvas.style.backgroundImage = "url('../../images/cat-2.jpg')";
+        }
+        gridCanvas.style.backgroundSize = "contain";
+    } else if (bear.some(keyword => inputValue.includes(keyword))) {
+        randomSelect = Math.floor(Math.random() * 8);
+        if (randomSelect === 7) {
+            gridCanvas.style.background = "url('../../images/joselyn-1.jpg')";
+        } else {
+            gridCanvas.style.backgroundImage = "url('../../images/bear.jpg')";
+        }
+        gridCanvas.style.backgroundSize = "contain";
+    } else if (wolf.some(keyword => inputValue.includes(keyword))) {
+        gridCanvas.style.backgroundImage = "url('../../images/wolf.jpg')";
+        gridCanvas.style.backgroundSize = "contain";
+    } else if (fish.some(keyword => inputValue.includes(keyword))) {
+        gridCanvas.style.backgroundImage = "url('../../images/fish.jpg')";
+        gridCanvas.style.backgroundSize = "contain";
+    } else if (bestFriends.some(keyword => inputValue.includes(keyword))) {
+        randomSelect = Math.floor(Math.random() * 2);
+        if (randomSelect === 0) {
+            gridCanvas.style.backgroundImage = "url('../../images/bff.jpg')";
+        } else {
+            gridCanvas.style.backgroundImage = "url('../../images/bff-2.jpg')";
+        }
+        gridCanvas.style.backgroundSize = "contain";
+    } else if (coolDude.some(keyword => inputValue.includes(keyword))) {
+        gridCanvas.style.backgroundImage = "url('../../images/cool-dude.jpg')";
+        gridCanvas.style.backgroundSize = "contain";
+    } else if (family.some(keyword => inputValue.includes(keyword))) {
+        randomSelect = Math.floor(Math.random() * 2)
+        if (randomSelect === 0) {
+            gridCanvas.style.backgroundImage = "url('../../images/family.jpg')";
+        } else {
+            gridCanvas.style.backgroundImage = "url('../../images/family-2.jpg')";
+        }
+        gridCanvas.style.backgroundSize = "contain";
+    } else if (guns.some(keyword => inputValue.includes(keyword))) {
+        randomSelect = Math.floor(Math.random() * 2);
+        if (randomSelect === 0) {
+            gridCanvas.style.backgroundImage = "url('../../images/guns-1.jpg')";
+        } else {
+            gridCanvas.style.backgroundImage = "url('../../images/guns-2.jpg')";
+        }
+        gridCanvas.style.backgroundSize = "contain";
+    } else if (nature.some(keyword => inputValue.includes(keyword))) {
+        randomSelect = Math.floor(Math.random() * 6);
+        if (randomSelect === 0) {
+            gridCanvas.style.backgroundImage = "url('../../images/nature-1.jpg')";
+        } else if (randomSelect === 1) {
+            gridCanvas.style.backgroundImage = "url('../../images/nature-2.jpg')";
+        } else if (randomSelect === 2) {
+            gridCanvas.style.backgroundImage = "url('../../images/nature-3.jpg')";
+        } else if (randomSelect === 3) {
+            gridCanvas.style.backgroundImage = "url('../../images/nature-4.jpg')";
+        } else if (randomSelect === 4) {
+            gridCanvas.style.backgroundImage = "url('../../images/lake.jpg')";
+        } else if (randomSelect === 5) {
+            gridCanvas.style.backgroundImage = "url('../../images/fish.jpg')";
+        } 
+        gridCanvas.style.backgroundSize = "contain";
+    } else if (prettyLady.some(keyword => inputValue.includes(keyword))) {
+        gridCanvas.style.backgroundImage = "url('../../images/pretty.jpg')";
+        gridCanvas.style.backgroundSize = "contain";
+    } else if (animal.some(keyword => inputValue.includes(keyword))) {
+        randomSelect = Math.floor(Math.random() * 9);
+        if (randomSelect === 0) {
+            gridCanvas.style.backgroundImage = "url('../../images/bear.jpg')";
+        } else if (randomSelect === 1) {
+            gridCanvas.style.backgroundImage = "url('../../images/cat-1.jpg')";
+        } else if (randomSelect === 2) {
+            gridCanvas.style.backgroundImage = "url('../../images/cat-2.jpg')";
+        } else if (randomSelect === 3) {
+            gridCanvas.style.backgroundImage = "url('../../images/dog-1.jpg')";
+        } else if (randomSelect === 4) {
+            gridCanvas.style.backgroundImage = "url('../../images/dog-2.jpg')";
+        } else if (randomSelect === 5) {
+            gridCanvas.style.backgroundImage = "url('../../images/fish.jpg')";
+        } else if (randomSelect === 6) {
+            gridCanvas.style.backgroundImage = "url('../../images/joselyn-daisy.jpg')";
+        } else if (randomSelect === 7) {
+            gridCanvas.style.backgroundImage = "url('../../images/joselyn-daisy-2.jpg')";
+        } else {
+            gridCanvas.style.backgroundImage = "url('../../images/wolf.jpg')";
+        } 
+        gridCanvas.style.backgroundSize = "contain";
+    } else if (inputValue === daisy) {
+        randomSelect = Math.floor(Math.random() * 2);
+        if (randomSelect === 0) {
+            gridCanvas.style.backgroundImage = "url('../../images/daisy-1.jpg')";
+        } else {
+            gridCanvas.style.backgroundImage = "url('../../images/daisy-2.jpg')";
+        }
+        gridCanvas.style.backgroundSize = "contain";
+    } else if (inputValue === joselyn) {
+        randomSelect = Math.floor(Math.random() * 3);
+        if (randomSelect === 0) {
+            gridCanvas.style.backgroundImage = "url('../../images/joselyn-1.jpg')";
+        } else if (randomSelect === 1) {
+            gridCanvas.style.backgroundImage = "url('../../images/joselyn-2.jpg')";
+        } else {
+            gridCanvas.style.backgroundImage = "url('../../images/joselyn-3.jpg')";
+        }
+        gridCanvas.style.backgroundSize = "contain";
     } else if (inputValue !== "") {
         picasso();
     }
 }
 
 
+const textField = document.querySelector(".prompt-text");
+const promptForm = document.querySelector(".prompt-form");
+
 promptForm.addEventListener('submit', handleForm);
+
+
+
+
+
+// // Submit Prompt
+// function handleForm(event) {
+//     event.preventDefault();
+//     const inputValue = textField.value.toLowerCase(); // Convert the input to lowercase for case-insensitive comparison
+//     if (inputValue === "mona lisa") {
+//         gridCanvas.innerHTML = "";
+//         gridCanvas.style.backgroundImage = "url('../../images/mona-lisa.jpg')";
+//         gridCanvas.style.backgroundSize = "contain";
+//     } else if (inputValue === "dog") {
+//         gridCanvas.innerHTML = "";
+//         gridCanvas.style.backgroundImage = "url('../../images/dog-1.jpg')";
+//         gridCanvas.style.backgroundSize = "contain";  // Fix here
+//     } else if (inputValue === "dogs" || inputValue === "cute") {
+//         gridCanvas.innerHTML = "";
+//         gridCanvas.style.backgroundImage = "url('../../images/joselyn-daisy.jpg')";
+//         gridCanvas.style.backgroundSize = "contain";  // Fix here
+//     } else if (inputValue === "cat" || inputValue === "kitty") {
+//         gridCanvas.innerHTML = "";
+//         gridCanvas.style.backgroundImage = "url('../../images/cat-2.jpg')";
+//         gridCanvas.style.backgroundSize = "contain";  // Fix here
+//     } else if (inputValue === "handsome" || inputValue === "hot") {
+//         gridCanvas.innerHTML = "";
+//         gridCanvas.style.backgroundImage = "url('../../images/handsome.jpg')";
+//         gridCanvas.style.backgroundSize = "contain";  // Fix here
+//     } else if (inputValue === "bff" || inputValue === "friends") {
+//         gridCanvas.innerHTML = "";
+//         gridCanvas.style.backgroundImage = "url('../../images/bff.jpg')";
+//         gridCanvas.style.backgroundSize = "contain";  // Fix here
+//     } else if (inputValue === "love" || inputValue === "family") {
+//         gridCanvas.innerHTML = "";
+//         gridCanvas.style.backgroundImage = "url('../../images/family.jpg')";
+//         gridCanvas.style.backgroundSize = "contain";  // Fix here
+//     } else if (inputValue !== "") {
+//         picasso();
+//     }
+// }
+
+
+
 
 
